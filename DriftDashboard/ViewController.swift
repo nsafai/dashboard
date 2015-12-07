eaf679680a75c48fd94e0635565f48aa4a895ab4
nsafai <nicolaisafai@gmail.com>
2015-12-07 02:01:48 -0800

    Initial Commit

---
+++ DriftDashboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DriftDashboard
+//
+//  Created by Nicolai Safai on 12/7/15.
+//  Copyright © 2015 Lime. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
